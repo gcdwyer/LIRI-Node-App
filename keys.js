413e568c77104f90b953bbd9caedc0730e20dbd9
Geoff Dwyer <geoff.dwyer@corpnet.com>
2017-10-04 16:58:57 -0400

    basic setup and some pseudo code

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'zbSVyY8DDQx7AwjD42WzRL5eG',
+  consumer_secret: 'S3ZIG8gfyoS37yNfG9BfVvWAvj6s1ht3dPlI3XTSbQ2QLmnbIZ',
+  access_token_key: '914694076225785856-4Umc9Uf8oRIVQlJSbo1VnatRjXJhXjs',
+  access_token_secret: 'Q9cvfFgbvrKtq0kMoWQF0n7hM6QRYEODtBM0LVqunw2VG',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,35 @@
+var twitterkeys = require(./keys.js);
+
+
+
+var myArgs = process.argv[2];
+
+
+
+if (myArgs === "my-tweets") {
+
+	// Display last 20 tweets and when they were created
+
+}
+
+if (myArgs === "spotify-this-song") {
+
+	// Display artist, song name, link and album
+
+	// if no song provided, play Ace of Base
+
+}
+
+if (myArgs === "movie-this") {
+
+	// Display movie title, year, rating, rotten rating, country, language, plot and actors
+
+	// if no movie provided, output Mr Nobody
+
+}
+
+if (myArgs === "do-what-it-says") {
+
+	// use fs node package and take text from random.txt then use it on one of liri's commands
+
+}
